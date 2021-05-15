b9a897e888389340c9736112450c42894fd556e0
crearesearch <crea@ub.edu>
2021-05-15 02:18:35 +0200

    rotated image

--- main.py
+++ main.py
@@ -87,26 +87,38 @@ def generate_mapping_data(image_width):
     return map_x_32, map_y_32
  
 
-def panorama_to_cubemap(inp_image):
+def panorama_to_cubemap(inp_image, out_dir='out'):
     t0 = time.time()
     imgIn = Image.open(inp_image)
     inSize = imgIn.size
     
     map_x_32, map_y_32 = generate_mapping_data(inSize[0])
     cubemap = cv2.remap(numpy.array(imgIn), map_x_32, map_y_32, cv2.INTER_LINEAR)
-    
+
     imgOut = Image.fromarray(cubemap)
-    out_filename = inp_image.split('/')[-1].split('.')[0]+"_out.png" 
+    out_filename = os.path.join(out_dir, inp_image.split('/')[-1].split('.')[0]+"_out1.png") 
     imgOut.save(out_filename)
+
+    # Mateixa foto però una mica rotada
+    h = cubemap.shape[0]
+    w = cubemap.shape[1]
+    left_cols = cubemap[:, 0:int(w/4)].copy()
+    cubemap[:, 0:int(w*3/4)] = cubemap[:, int(w/4):int(w)]
+    cubemap[:, int(w*3/4):int(w)] = left_cols
+    imgOut = Image.fromarray(cubemap)
+    out_filename = os.path.join(out_dir, inp_image.split('/')[-1].split('.')[0]+"_out2.png") 
+    imgOut.save(out_filename)
+
+
     print(time.time() - t0, 'seconds')
     print('File generated:', out_filename)
     return out_filename
 
 
-def files_to_panorama(front, back, right, left, top, bottom):
+def files_to_panorama(front, back, right, left, top, bottom, out_dir='out'):
     t0 = time.time()
 
-    output_filename = f'{int(time.time())}'
+    output_filename = os.path.join(out_dir, f'{int(time.time())}')
     command = f'cube2sphere {front} {back} {right} {left} {top} {bottom} -f PNG -o {output_filename}'
     os.system(command)
 
@@ -120,7 +132,7 @@ def show_im(image):
     cv2.imshow('shit', image)
     cv2.waitKey()
 
-def cubemap_to_6_files(fname):
+def cubemap_to_6_files(fname, out_dir='out'):
     file_name = fname.split('/')[-1].split('.')[0]
     image = cv2.imread(fname)
     h = image.shape[0]
@@ -133,18 +145,20 @@ def cubemap_to_6_files(fname):
     top     = image[0:int(h/3), int(w*2/4):int(w*3/4)]
     bottom  = image[int(h*2/3):int(h), int(w*2/4):int(w*3/4)]
 
-    cv2.imwrite('back.png', back)
-    cv2.imwrite('left.png', left)
-    cv2.imwrite('front.png', front)
-    cv2.imwrite('right.png', right)
-    cv2.imwrite('top.png', top)
-    cv2.imwrite('bottom.png', bottom)
+    cv2.imwrite(os.path.join(out_dir, 'back.png'), back)
+    cv2.imwrite(os.path.join(out_dir, 'left.png'), left)
+    cv2.imwrite(os.path.join(out_dir, 'front.png'), front)
+    cv2.imwrite(os.path.join(out_dir, 'right.png'), right)
+    cv2.imwrite(os.path.join(out_dir, 'top.png'), top)
+    cv2.imwrite(os.path.join(out_dir, 'bottom.png'), bottom)
 
 
 if __name__ == "__main__":
-    fname = 'tours_1/905/l_r_equirectangular_905_59bf9b6637fd6.jpeg'
+    fname = 'prova.jpeg'
 
-    # panorama_to_cubemap(fname)
+    folder_id = '../tours_1/427083/'
 
-    cubemap_to_6_files('prova_out.png')
-    files_to_panorama('back.png', 'front.png', 'right.png','left.png', 'top.png', 'bottom.png')
+    panorama_to_cubemap(fname)
+
+    # cubemap_to_6_files('prova_out.png')
+    # files_to_panorama('back.png', 'front.png', 'right.png','left.png', 'top.png', 'bottom.png')
